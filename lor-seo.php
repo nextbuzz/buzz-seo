f1c7751c9a3644e5091c5af98a89d5cbc9e2deb5
Bas de Kort <bas@nextbuzz.nl>
2016-01-03 14:45:04 +0100

    Add: required plugin files.

---
+++ lor-seo.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+Plugin Name: LengthOfRope SEO
+Plugin URI: http://www.lengthofrope.nl
+Description: This might become a simple WordPress SEO plugin someday. Currently in active development.
+Version: 0.0.1
+Author: LengthOfRope
+Author URI: https://github.com/lengthofrope/
+License: MIT
+Text Domain: lor-seo
+*/
+
+// Make sure we don't expose any info if called directly
+if (!function_exists( 'add_action' )) {
+	echo 'Silence is golden.';
+	exit;
+}
