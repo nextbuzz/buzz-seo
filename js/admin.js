99d0efb8b241549d9fe871efae046a8c536504b7
Bas de Kort <bdekort@gmail.com>
2016-01-05 10:27:51 +0100

    Fix: make sure tabs JS can be used multiple times

--- js/admin.js
+++ js/admin.js
@@ -2,45 +2,48 @@
     'use strict';
 
     $(function () {
-        var navTabs = $('#lor-seo-tabs').children('.nav-tab-wrapper'),
+        var tabContainer = $('.lor-seo-tabs');
+        tabContainer.each(function () {
+            var that = $(this), navTabs = that.children('.nav-tab-wrapper'),
                 tabIndex = null;
 
-        /* Whenever each of the navigation tabs is clicked, check to see if it has the 'nav-tab-active'
-         * class name. If not, then mark it as active; otherwise, don't do anything (as it's already
-         * marked as active.
-         *
-         * Next, when a new tab is marked as active, the corresponding child view needs to be marked
-         * as visible. We do this by toggling the 'hidden' class attribute of the corresponding variables.
-         */
-        navTabs.children().each(function () {
-            $(this).on('click', function (evt) {
+            /* Whenever each of the navigation tabs is clicked, check to see if it has the 'nav-tab-active'
+             * class name. If not, then mark it as active; otherwise, don't do anything (as it's already
+             * marked as active.
+             *
+             * Next, when a new tab is marked as active, the corresponding child view needs to be marked
+             * as visible. We do this by toggling the 'hidden' class attribute of the corresponding variables.
+             */
+            navTabs.children().each(function () {
+                $(this).on('click', function (evt) {
 
-                evt.preventDefault();
+                    evt.preventDefault();
 
-                // If this tab is not active...
-                if (!$(this).hasClass('nav-tab-active')) {
+                    // If this tab is not active...
+                    if (!$(this).hasClass('nav-tab-active')) {
 
-                    // Unmark the current tab and mark the new one as active
-                    $('.nav-tab-active').removeClass('nav-tab-active');
-                    $(this).addClass('nav-tab-active');
+                        // Unmark the current tab and mark the new one as active
+                        $('.nav-tab-active').removeClass('nav-tab-active');
+                        $(this).addClass('nav-tab-active');
 
-                    // Save the index of the tab that's just been marked as active. It will be 0 - 3.
-                    tabIndex = $(this).index();
+                        // Save the index of the tab that's just been marked as active. It will be 0 - 3.
+                        tabIndex = $(this).index();
 
-                    // Hide the old active content
-                    $('#lor-seo-tabs')
+                        // Hide the old active content
+                        that
                             .children('div:not( .inside.hidden )')
                             .addClass('hidden');
 
-                    $('#lor-seo-tabs')
+                        that
                             .children('div:nth-child(' + (tabIndex) + ')')
                             .addClass('hidden');
 
-                    // And display the new content
-                    $('#lor-seo-tabs')
+                        // And display the new content
+                        that
                             .children('div:nth-child( ' + (tabIndex + 2) + ')')
                             .removeClass('hidden');
-                }
+                    }
+                });
             });
         });
     });
