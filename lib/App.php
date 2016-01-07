c010c557a4e2a51124287dea532213302f7fd938
Bas de Kort <bdekort@gmail.com>
2016-01-07 15:46:04 +0100

    Add: Dutch translations

--- lib/App.php
+++ lib/App.php
@@ -12,6 +12,9 @@ class App
 
     public function __construct()
     {
+        // Just an easy way to rewrite the POT file.
+        //$this->rewritePOT();
+
         // Always init
         add_action('plugins_loaded', array($this, 'pluginsLoaded'));
         add_action('admin_enqueue_scripts', array($this, 'enqueueAdminScripts'));
@@ -68,106 +71,116 @@ class App
         wp_localize_script('buzz-seo-admin-js', 'LORSEOData', array(
             'Analysis' => array(
                 array(
-                    'id' => 'wordCount',
+                    'id'   => 'wordCount',
                     'data' => array(
                         array(
-                            'min' => 300,
+                            'min'   => 300,
                             'score' => 10,
-                            'text' => __('The text contains {0} words, this is more than the {1} word recommended minimum.', 'buzz-seo')
+                            'text'  => __('The text contains {0} words, this is more than the {1} word recommended minimum.', 'buzz-seo')
                         ),
                         array(
-                            'min' => 250,
-                            'max' => 299,
+                            'min'   => 250,
+                            'max'   => 299,
                             'score' => 7,
-                            'text' => __('The text contains {0} words, this is slightly below the {1} word recommended minimum. Add a bit more copy.', 'buzz-seo')
+                            'text'  => __('The text contains {0} words, this is slightly below the {1} word recommended minimum. Add a bit more copy.', 'buzz-seo')
                         ),
                         array(
-                            'min' => 200,
-                            'max' => 249,
+                            'min'   => 200,
+                            'max'   => 249,
                             'score' => 5,
-                            'text' => __('The text contains {0} words, this is below the {1} word recommended minimum. Add a little more useful content on this topic for readers.', 'buzz-seo')
+                            'text'  => __('The text contains {0} words, this is below the {1} word recommended minimum. Add a little more useful content on this topic for readers.', 'buzz-seo')
                         ),
                         array(
-                            'min' => 100,
-                            'max' => 199,
+                            'min'   => 100,
+                            'max'   => 199,
                             'score' => 3,
-                            'text' => __('The text contains {0} words, this is below the {1} word recommended minimum. Add more useful content on this topic for readers.', 'buzz-seo')
+                            'text'  => __('The text contains {0} words, this is below the {1} word recommended minimum. Add more useful content on this topic for readers.', 'buzz-seo')
                         ),
                         array(
-                            'min' => 0,
-                            'max' => 99,
+                            'min'   => 0,
+                            'max'   => 99,
                             'score' => 1,
-                            'text' => __('The text contains {0} word(s). This is far too low and should be increased.', 'buzz-seo')
+                            'text'  => __('The text contains {0} word(s). This is far too low and should be increased.', 'buzz-seo')
                         )
                     )
                 ),
                 array(
-                    'id' => 'metaDescriptionLength',
+                    'id'   => 'metaDescriptionLength',
                     'data' => array(
                         array(
-                            'min' => 158,
+                            'min'   => 158,
                             'score' => 5,
-                            'text' => __('The specified meta description is {2} character(s) over the {1} available. Reducing it will ensure the entire description is visible.', 'buzz-seo')
+                            'text'  => __('The specified meta description is {2} character(s) over the {1} available. Reducing it will ensure the entire description is visible.', 'buzz-seo')
                         ),
                         array(
-                            'min' => 0,
-                            'max' => 0,
+                            'min'   => 0,
+                            'max'   => 0,
                             'score' => 1,
-                            'text' => __('No meta description has been specified, search engines will display copy from the page instead.', 'buzz-seo')
+                            'text'  => __('No meta description has been specified, search engines will display copy from the page instead.', 'buzz-seo')
                         ),
                         array(
-                            'min' => 1,
-                            'max' => 120,
+                            'min'   => 1,
+                            'max'   => 120,
                             'score' => 6,
-                            'text' => __('The meta description is {0} character(s), however up to {1} characters are available.', 'buzz-seo')
+                            'text'  => __('The meta description is {0} character(s), however up to {1} characters are available.', 'buzz-seo')
                         ),
                         array(
-                            'min' => 121,
-                            'max' => 157,
+                            'min'   => 121,
+                            'max'   => 157,
                             'score' => 10,
-                            'text' => __('The meta description has an optimal length of {0} out of max {1} characters.', 'buzz-seo')
+                            'text'  => __('The meta description has an optimal length of {0} out of max {1} characters.', 'buzz-seo')
                         )
                     )
                 ),
                 array(
-                    'id' => 'subHeadings',
+                    'id'   => 'subHeadings',
                     'data' => array(
                         array(
-                            'max' => 0,
+                            'max'   => 0,
                             'score' => 1,
-                            'text' => __('No subheading tags (like an H2) appear in the copy.', 'buzz-seo')
+                            'text'  => __('No subheading tags (like an H2) appear in the copy.', 'buzz-seo')
                         ),
                         array(
-                            'min' => 1,
+                            'min'   => 1,
                             'score' => 10,
-                            'text' => __('Subheadings appear in the copy.', 'buzz-seo')
+                            'text'  => __('Subheadings appear in the copy.', 'buzz-seo')
                         )
                     )
                 ),
                 array(
-                    'id' => 'pageTitleLength',
+                    'id'   => 'pageTitleLength',
                     'data' => array(
                         array(
-                            'max' => 0,
+                            'max'   => 0,
                             'score' => 1,
-                            'text' => __('Please create a page title.', 'buzz-seo')
+                            'text'  => __('Please create a page title.', 'buzz-seo')
                         ),
                         array(
-                            'min' => 1,
-                            'max' => 39,
+                            'min'   => 1,
+                            'max'   => 39,
                             'score' => 6,
-                            'text' => __('The page title contains %3$d characters, which is less than the recommended minimum of %1$d characters.', 'buzz-seo')
+                            'text'  => __('The page title contains {2} characters, which is less than the recommended minimum of {0} characters.', 'buzz-seo')
                         ),
                         array(
-                            'min' => 40,
-                            'max' => 70,
+                            'min'   => 40,
+                            'max'   => 70,
                             'score' => 9,
-                            'text' => __('The page title is between the %1$d character minimum and the recommended %2$d character maximum.', 'buzz-seo')
+                            'text'  => __('The page title is between the {0} character minimum and the recommended {1} character maximum.', 'buzz-seo')
                         )
                     )
                 ),
             )
         ));
+    }
+
+    private function rewritePOT()
+    {
+        $Pot = new \NextBuzz\SEO\Tools\CreatePot();
+        $Pot->addPath(BUZZSEO_DIR . 'tal/', "xml")
+            ->addPath(BUZZSEO_DIR . 'lib/', "php")
+            ->stripPathInComments(BUZZSEO_DIR)
+            ->parse()
+            ->writePot(BUZZSEO_DIR . 'languages/buzz-seo.pot');
     }
 
 }
