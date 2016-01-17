5b3db54389fbb69f6448a0f6523135283fff5d79
Bas de Kort <bdekort@gmail.com>
2016-01-17 22:39:03 +0100

    Start: XML output

--- lib/Features/Sitemaps.php
+++ lib/Features/Sitemaps.php
@@ -22,18 +22,61 @@ class Sitemaps extends BaseFeature
 
     public function init()
     {
-        add_action('admin_menu', array(&$this, 'createAdminMenu'));
+        add_action('admin_menu', array($this, 'createAdminMenu'));
+        add_action('pre_get_posts', array($this, 'checkRenderSitemap'), 1);
     }
 
+    /**
+     * Add the administrator submenu
+     */
     public function createAdminMenu()
     {
         // Add Settings Sub Option Page
         add_submenu_page('BuzzSEO', __('XML Sitemaps', 'buzz-seo'), __('XML Sitemaps', 'buzz-seo'), 'edit_posts', 'XMLSitemaps', array($this, "addAdminUI"));
     }
 
+    /**
+     * Create the admin interface pages.
+     */
     public function addAdminUI()
     {
         \NextBuzz\SEO\PHPTAL\Settings\Sitemaps::factory()->render();
+    }
+
+    /**
+     * Render the xml output if requested.
+     */
+    public function checkRenderSitemap($query)
+    {
+        // If not the main query, we don't need to check if output is requested
+        if (!$query->is_main_query()) {
+            return;
+        }
+        
+        // TODO: Somehow detect if we are a sitemap xml page
+        
+        if (!headers_sent()) {
+            header($this->getHTTPProtocol() . ' 200 OK', true, 200);
+            // Prevent the search engines from indexing the XML Sitemap.
+            header('X-Robots-Tag: noindex, follow', true);
+            header('Content-Type: text/xml');
+        }
+        
+        
+
+        // Make sure WordPress does not output any footer content
+        remove_all_actions('wp_footer');
+        exit();
+    }
+
+    /**
+     * Get the output protocol. Defaults to HTTP/1.1 if none found
+     * 
+     * @return string
+     */
+    private function getHTTPProtocol()
+    {
+        return (isset($_SERVER['SERVER_PROTOCOL']) && $_SERVER['SERVER_PROTOCOL'] !== '') ? sanitize_text_field($_SERVER['SERVER_PROTOCOL']) : 'HTTP/1.1';
     }
 
 }
