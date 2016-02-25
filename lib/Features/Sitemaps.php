1cc46efc5f9d1da58605f15631bf1cd38740b3c0
Bas de Kort <bas@nextbuzz.nl>
2016-02-25 11:44:33 +0100

    Fixed an issue with the sitemap generator if no items where selected. Bump versionnumbers

--- lib/Features/Sitemaps.php
+++ lib/Features/Sitemaps.php
@@ -105,7 +105,7 @@ class Sitemaps extends BaseFeature
         }
 
         // Get sitemap data and set some default options
-        $options = get_option('_settingsSettingsXML', array(
+        $options      = get_option('_settingsSettingsXML', array(
             'itemsperpage' => 250
         ));
         $itemsperpage = $options['itemsperpage'];
@@ -118,50 +118,49 @@ class Sitemaps extends BaseFeature
         if ($type === "1") {
             // Build the sitemapindex
             // Get all data
-            foreach ($options['posttypes'] as $posttype => $value)
-            {
-                // Get pagination count for post type (and just return ids, since we just want a count)
-                $numposts = count(get_posts(array(
-                    'post_type' => $posttype,
-                    'posts_per_page' => -1,
-                    'cache_results' => false,
-                    'fields' => 'ids'
-                )));
-                $pages = ceil($numposts / $itemsperpage);
-                for ($p = 1; $p <= $pages; $p++)
-                {
-                    $talData[] = array(
-                        'loc' => $baseurl . 'sitemap-' . $posttype . '-' . $p . '.xml',
-                        'lastmod' => $this->getLastModifiedDate($posttype, $p, $itemsperpage),
-                    );
+            if (isset($options['posttypes']) && is_array($options['posttypes'])) {
+                foreach ($options['posttypes'] as $posttype => $value) {
+                    // Get pagination count for post type (and just return ids, since we just want a count)
+                    $numposts = count(get_posts(array(
+                        'post_type'      => $posttype,
+                        'posts_per_page' => -1,
+                        'cache_results'  => false,
+                        'fields'         => 'ids'
+                    )));
+                    $pages    = ceil($numposts / $itemsperpage);
+                    for ($p = 1; $p <= $pages; $p++) {
+                        $talData[] = array(
+                            'loc'     => $baseurl . 'sitemap-' . $posttype . '-' . $p . '.xml',
+                            'lastmod' => $this->getLastModifiedDate($posttype, $p, $itemsperpage),
+                        );
+                    }
                 }
             }
-            foreach ($options['taxonomies'] as $taxonomy => $value)
-            {
-                // Get taxonomies
-                $numposts = wp_count_terms($taxonomy, array('hide_empty' => true));
-                $pages = ceil($numposts / $itemsperpage);
-                for ($p = 1; $p <= $pages; $p++)
-                {
-                    $talData[] = array(
-                        'loc' => $baseurl . 'sitemap-' . $taxonomy . '-' . $p . '.xml',
-                        'lastmod' => $this->getLastModifiedDateTax($taxonomy, $p, $itemsperpage),
-                    );
+            if (isset($options['taxonomies']) && is_array($options['taxonomies'])) {
+                foreach ($options['taxonomies'] as $taxonomy => $value) {
+                    // Get taxonomies
+                    $numposts = wp_count_terms($taxonomy, array('hide_empty' => true));
+                    $pages    = ceil($numposts / $itemsperpage);
+                    for ($p = 1; $p <= $pages; $p++) {
+                        $talData[] = array(
+                            'loc'     => $baseurl . 'sitemap-' . $taxonomy . '-' . $p . '.xml',
+                            'lastmod' => $this->getLastModifiedDateTax($taxonomy, $p, $itemsperpage),
+                        );
+                    }
                 }
             }
 
             // Add author map
-            if ($options['includeauthor']) {
-                $authors = get_users(array('who' => 'authors'));
-                $users = wp_list_pluck($authors, 'ID');
+            if (isset($options['includeauthor']) && $options['includeauthor']) {
+                $authors         = get_users(array('who' => 'authors'));
+                $users           = wp_list_pluck($authors, 'ID');
                 $latestUserStamp = max(wp_list_pluck($authors, 'user_registered'));
 
                 $numusers = count($users);
-                $pages = ceil($numusers / $itemsperpage);
-                for ($p = 1; $p <= $pages; $p++)
-                {
+                $pages    = ceil($numusers / $itemsperpage);
+                for ($p = 1; $p <= $pages; $p++) {
                     $talData[] = array(
-                        'loc' => $baseurl . 'sitemap-author-' . $p . '.xml',
+                        'loc'     => $baseurl . 'sitemap-author-' . $p . '.xml',
                         'lastmod' => \NextBuzz\SEO\Date\Timezone::dateFromTimestamp($latestUserStamp),
                     );
                 }
@@ -184,21 +183,20 @@ class Sitemaps extends BaseFeature
             // Get all data
             $users = get_users(array('who' => 'authors'));
             $users = array_splice($users, (($page - 1) * $itemsperpage), $itemsperpage);
-            foreach ($users as $user)
-            {
+            foreach ($users as $user) {
                 $latestPost = get_posts(array(
                     'posts_per_page' => '1',
-                    'author' => $user->ID,
-                    'orderby' => 'modified',
-                    'order' => 'DESC'
+                    'author'         => $user->ID,
+                    'orderby'        => 'modified',
+                    'order'          => 'DESC'
                 ));
 
-                $modified = is_array($latestPost) && isset($latestPost[0]) ? $latestPost[0]->post_modified_gmt : $user->user_registered;
+                $modified  = is_array($latestPost) && isset($latestPost[0]) ? $latestPost[0]->post_modified_gmt : $user->user_registered;
                 $talData[] = array(
-                    'loc' => get_author_posts_url($user->ID),
-                    'lastmod' => \NextBuzz\SEO\Date\Timezone::dateFromTimestamp($modified),
+                    'loc'        => get_author_posts_url($user->ID),
+                    'lastmod'    => \NextBuzz\SEO\Date\Timezone::dateFromTimestamp($modified),
                     'changefreq' => 'weekly',
-                    'priority' => 0.8,
+                    'priority'   => 0.8,
                 );
             }
 
@@ -221,31 +219,29 @@ class Sitemaps extends BaseFeature
             if (in_array($type, $postTypes)) {
                 // Get post type posts
                 $posts = get_posts(array(
-                    'post_type' => $type,
+                    'post_type'      => $type,
                     'posts_per_page' => $itemsperpage,
-                    'paged' => $page,
-                    'orderby' => 'modified',
-                    'order' => 'DESC'
+                    'paged'          => $page,
+                    'orderby'        => 'modified',
+                    'order'          => 'DESC'
                 ));
 
                 $frontId = intval(get_option('page_on_front', -1));
-                $blogId = intval(get_option('page_for_posts', -1));
+                $blogId  = intval(get_option('page_for_posts', -1));
 
-                foreach ($posts as $post)
-                {
+                foreach ($posts as $post) {
                     $talData[] = array(
-                        'loc' => get_permalink($post->ID),
-                        'lastmod' => \NextBuzz\SEO\Date\Timezone::dateFromTimestamp($post->post_modified_gmt),
+                        'loc'        => get_permalink($post->ID),
+                        'lastmod'    => \NextBuzz\SEO\Date\Timezone::dateFromTimestamp($post->post_modified_gmt),
                         'changefreq' => ($post->ID === $frontId || $post->ID === $blogId) ? 'daily' : 'monthly',
-                        'priority' => ($post->ID === $frontId || $post->ID === $blogId) ? 1.0 : 0.8,
+                        'priority'   => ($post->ID === $frontId || $post->ID === $blogId) ? 1.0 : 0.8,
                     );
                 }
             } else {
                 // Get taxonomy posts
                 $terms = get_terms($type);
                 $terms = array_splice($terms, (($page - 1) * $itemsperpage), $itemsperpage);
-                foreach ($terms as $term)
-                {
+                foreach ($terms as $term) {
                     $priority = 0.2;
                     if ($term->count >= 3) {
                         $priority = 0.4;
@@ -254,10 +250,10 @@ class Sitemaps extends BaseFeature
                         $priority = 0.6;
                     }
                     $talData[] = array(
-                        'loc' => get_term_link($term->term_id),
-                        'lastmod' => $this->getLastModifiedDateTerm($type, $term->term_id),
+                        'loc'        => get_term_link($term->term_id),
+                        'lastmod'    => $this->getLastModifiedDateTerm($type, $term->term_id),
                         'changefreq' => 'weekly',
-                        'priority' => $priority,
+                        'priority'   => $priority,
                     );
                 }
             }
@@ -350,22 +346,21 @@ class Sitemaps extends BaseFeature
         $terms = get_terms($taxonomy);
 
         $termids = array();
-        foreach ($terms as $term)
-        {
+        foreach ($terms as $term) {
             $termids[] = intval($term->term_id);
         }
 
         $results = get_posts(
             array(
                 'posts_per_page' => 1,
-                'orderby' => 'modified',
-                'order' => 'DESC',
-                'tax_query' => array(
+                'orderby'        => 'modified',
+                'order'          => 'DESC',
+                'tax_query'      => array(
                     array(
                         'taxonomy' => $taxonomy,
-                        'field' => 'term_id',
+                        'field'    => 'term_id',
                         'operator' => 'IN',
-                        'terms' => $termids
+                        'terms'    => $termids
                     )
                 )
             )
@@ -391,20 +386,19 @@ class Sitemaps extends BaseFeature
         $terms = get_terms($taxonomy);
 
         $termids = array();
-        foreach ($terms as $term)
-        {
+        foreach ($terms as $term) {
             $termids[] = intval($term->term_id);
         }
 
         $results = get_posts(array(
             'posts_per_page' => 1,
-            'orderby' => 'modified',
-            'order' => 'DESC',
-            'tax_query' => array(
+            'orderby'        => 'modified',
+            'order'          => 'DESC',
+            'tax_query'      => array(
                 array(
-                    'taxonomy' => $taxonomy,
-                    'field' => 'id',
-                    'terms' => $termId,
+                    'taxonomy'         => $taxonomy,
+                    'field'            => 'id',
+                    'terms'            => $termId,
                     'include_children' => false
                 )
             )
