ec7d24f9831e8a4418dd9d1ab62a006f564993e7
Bas de Kort <bdekort@gmail.com>
2016-03-01 16:31:14 +0100

    Update: Settings interface Organizations

--- lib/PHPTAL/Settings/StructuredData.php
+++ lib/PHPTAL/Settings/StructuredData.php
@@ -15,56 +15,86 @@ class StructuredData extends \NextBuzz\SEO\PHPTAL\SettingsPage
         parent::__construct('SettingsStructuredData');
 
         // Creative Works
-        $creativeWorks = array(
-            'Article' => array(
-                'name' => __('Article', 'buzz-seo'),
-                'intro' => __('An article, such as a news article or piece of investigative report. Newspapers and magazines have articles of many different types and this is intended to cover them all.', 'buzz-seo'),
-                'addauthoroption' => true,
+        $tabs = array(
+            array(
+                'name' => __('Creative works', 'buzz-seo'),
+                'items' => array(
+                    'Article' => array(
+                        'name' => __('Article', 'buzz-seo'),
+                        'intro' => __('An article, such as a news article or piece of investigative report. Newspapers and magazines have articles of many different types and this is intended to cover them all.', 'buzz-seo'),
+                        'addauthoroption' => true,
+                    ),
+                    'BlogPosting' => array(
+                        'name' => __('Blog posting', 'buzz-seo'),
+                        'intro' => __('A blog post', 'buzz-seo'),
+                        'addauthoroption' => true,
+                    ),
+                    'Book' => array(
+                        'name' => __('Book', 'buzz-seo'),
+                        'intro' => __('A book', 'buzz-seo'),
+                        'addauthoroption' => true,
+                    ),
+                    'Code' => array(
+                        'name' => __('Code', 'buzz-seo'),
+                        'intro' => __('Computer programming source code. Example: Full (compile ready) solutions, code snippet samples, scripts, templates.', 'buzz-seo'),
+                        'addauthoroption' => true,
+                    ),
+                    'Game' => array(
+                        'name' => __('Game', 'buzz-seo'),
+                        'intro' => __('A game', 'buzz-seo'),
+                        'addauthoroption' => true,
+                    ),
+                    'Movie' => array(
+                        'name' => __('Movie', 'buzz-seo'),
+                        'intro' => __('A movie', 'buzz-seo'),
+                        'addauthoroption' => true,
+                    ),
+                    'MusicRecording' => array(
+                        'name' => __('Music recording', 'buzz-seo'),
+                        'intro' => __('A music recording (track), usually a single song.', 'buzz-seo'),
+                        'addauthoroption' => true,
+                    ),
+                    'Painting' => array(
+                        'name' => __('Painting', 'buzz-seo'),
+                        'intro' => __('A painting', 'buzz-seo'),
+                        'addauthoroption' => true,
+                    ),
+                    'Photograph' => array(
+                        'name' => __('Photograph', 'buzz-seo'),
+                        'intro' => __('A photograph', 'buzz-seo'),
+                        'addauthoroption' => true,
+                    ),
+                    'Recipe' => array(
+                        'name' => __('Recipe', 'buzz-seo'),
+                        'intro' => __('A recipe', 'buzz-seo'),
+                        'addauthoroption' => true,
+                    ),
+                ),
             ),
-            'BlogPosting' => array(
-                'name' => __('Blog posting', 'buzz-seo'),
-                'intro' => __('A blog post', 'buzz-seo'),
-                'addauthoroption' => true,
-            ),
-            'Book' => array(
-                'name' => __('Book', 'buzz-seo'),
-                'intro' => __('A book', 'buzz-seo'),
-                'addauthoroption' => true,
-            ),
-            'Code' => array(
-                'name' => __('Code', 'buzz-seo'),
-                'intro' => __('Computer programming source code. Example: Full (compile ready) solutions, code snippet samples, scripts, templates.', 'buzz-seo'),
-                'addauthoroption' => true,
-            ),
-            'Game' => array(
-                'name' => __('Game', 'buzz-seo'),
-                'intro' => __('A game', 'buzz-seo'),
-                'addauthoroption' => true,
-            ),
-            'Movie' => array(
-                'name' => __('Movie', 'buzz-seo'),
-                'intro' => __('A movie', 'buzz-seo'),
-                'addauthoroption' => true,
-            ),
-            'MusicRecording' => array(
-                'name' => __('Music recording', 'buzz-seo'),
-                'intro' => __('A music recording (track), usually a single song.', 'buzz-seo'),
-                'addauthoroption' => true,
-            ),
-            'Painting' =>  array(
-                'name' => __('Painting', 'buzz-seo'),
-                'intro' => __('A painting', 'buzz-seo'),
-                'addauthoroption' => true,
-            ),
-            'Photograph' => array(
-                'name' => __('Photograph', 'buzz-seo'),
-                'intro' => __('A photograph', 'buzz-seo'),
-                'addauthoroption' => true,
-            ),
-            'Recipe' => array(
-                'name' => __('Recipe', 'buzz-seo'),
-                'intro' => __('A recipe', 'buzz-seo'),
-                'addauthoroption' => true,
+            array(
+                'name' => __('Organizations', 'buzz-seo'),
+                'items' => array(
+                    'Organization' => array(
+                        'name' => __('Organization', 'buzz-seo'),
+                        'intro' => __('An organization such as a school, NGO, corporation, club, etc.', 'buzz-seo'),
+                        'addauthoroption' => false,
+                    ),
+                    'Corporation' => array(
+                        'name' => __('Corporation', 'buzz-seo'),
+                        'intro' => __('A business corporation.', 'buzz-seo'),
+                        'addauthoroption' => false,
+                    ),
+                    'LocalBusiness' => array(
+                        'name' => __('Local business', 'buzz-seo'),
+                        'intro' => __('A particular physical business or branch of an organization. Examples of LocalBusiness include a restaurant, a particular branch of a restaurant chain, a branch of a bank, a medical practice, a club, a bowling alley, etc.', 'buzz-seo'),
+                        'addauthoroption' => false,
+                    ),
+                    'SportsOrganization' => array(
+                        'name' => __('Sports organization', 'buzz-seo'),
+                        'intro' => __('Represents the collection of all sports organizations, including sports teams, governing bodies, and sports associations.', 'buzz-seo'),
+                        'addauthoroption' => false,
+                    )
+                ),
             ),
         );
 
@@ -78,7 +108,7 @@ class StructuredData extends \NextBuzz\SEO\PHPTAL\SettingsPage
 
         $this->setTalData(array(
             'postTypes' => $postTypes,
-            'creativeWorks' => $creativeWorks,
+            'tabs' => $tabs,
         ));
     }
 
