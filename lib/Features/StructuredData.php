15623a81eb0de934c3051a7cc56ff2c138978b9a
Bas de Kort <bdekort@gmail.com>
2016-03-16 05:15:36 +0100

    Move structured data for creative works to the loop to prevent queries early in the wordpress initialization that might not always be needed. This closes #29.

--- lib/Features/StructuredData.php
+++ lib/Features/StructuredData.php
@@ -29,6 +29,7 @@ class StructuredData extends BaseFeature
         add_action('current_screen', array($this, 'initBack'));
         add_action('admin_menu', array($this, 'createAdminMenu'));
         add_action('wp_head', array($this, 'addJSONLDToHead'), 1);
+        add_filter('the_content', array($this, 'addJSONLDToLoop'), 1);
     }
 
     /**
@@ -134,8 +135,26 @@ class StructuredData extends BaseFeature
             $Create->add($Org);
         }
 
+        if ($hasData) {
+            echo $Create->getJSONLDScript() . PHP_EOL;
+        }
+    }
+    
+    /**
+     * Output JSON-LD data in the loop of the HTML page if required
+     */
+    public function addJSONLDToLoop($content)
+    {
+        // Not a single page, bail early
+        if (!is_singular()) {
+            return $content;
+        }
+        $options = get_option('_settingsSettingsStructuredData', true);
+
+        $Create = \LengthOfRope\JSONLD\Create::factory();
+        $hasData = false;
         // Add Article
-        if (isset($options['addarticle']) && is_array($options['addarticle']) && is_singular()) {
+        if (isset($options['addarticle']) && is_array($options['addarticle'])) {
             $posttype = get_post_type();
             foreach ($options['addarticle'] as $creativeWorkType => $postTypes)
             {
@@ -146,21 +165,21 @@ class StructuredData extends BaseFeature
                 if (in_array($posttype, $addForPostTypes)) {
                     // We are in a posttype that we want to add the article data for, but since we are not in the loop, get
                     // the author data in an arbitrary way
-                    $post = get_post();
+                    $postId = get_the_ID();
 
                     $class = "\\LengthOfRope\\JSONLD\\Schema\\" . $creativeWorkType . "Schema";
                     $Schema = $class::factory();
 
                     if ($Schema instanceof Schema\ThingSchema) {
-                        $Schema->setName(get_the_title($post));
-                        $url = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'full');
+                        $Schema->setName(get_the_title());
+                        $url = wp_get_attachment_image_src(get_post_thumbnail_id(), 'full');
                         if (is_array($url)) {
                             $Schema->setImage(Schema\ImageObjectSchema::factory()->setUrl($url[0])->setWidth($url[1])->setHeight($url[2]));
                         } else {
                             // Skip this post since it has no image
                             continue;
                         }
-                        $excerpt = apply_filters('the_excerpt', get_post_field('post_excerpt', $post->ID));
+                        $excerpt = apply_filters('the_excerpt', get_post_field('post_excerpt', $postId));
                         if (!empty($excerpt)) {
                             $Schema->setDescription(strip_tags($excerpt));
                         }
@@ -169,20 +188,20 @@ class StructuredData extends BaseFeature
                     // Article and blog require a headline, we simply set it to the post title.
                     if ($Schema instanceof Schema\ArticleSchema ||
                         $Schema instanceof Schema\BlogPostingSchema) {
-                        $Schema->setHeadline(get_the_title($post));
+                        $Schema->setHeadline(get_the_title());
                     }
 
                     if ($Schema instanceof Schema\CreativeWorkSchema) {
-                        $Schema->setDatePublished(get_the_date('c'), $post);
-                        $Schema->setDateModified(get_the_modified_date('c'), $post);
+                        $Schema->setDatePublished(get_the_date('c'));
+                        $Schema->setDateModified(get_the_modified_date('c'));
                     }
 
                     // Add author
                     if (isset($options['addauthor']) && is_array($options['addauthor']) && isset($options['addauthor'][$creativeWorkType])) {
-                        $authorF = get_the_author_meta('first_name', $post->post_author);
-                        $authorL = get_the_author_meta('last_name', $post->post_author);
-                        $authormail = get_the_author_meta('email', $post->post_author);
-                        $authorurl = get_the_author_meta('url', $post->post_author);
+                        $authorF = get_the_author_meta('first_name');
+                        $authorL = get_the_author_meta('last_name');
+                        $authormail = get_the_author_meta('email');
+                        $authorurl = get_the_author_meta('url');
 
                         $Author = Schema\PersonSchema::factory();
                         if (!empty($authorF)) {
@@ -208,7 +227,7 @@ class StructuredData extends BaseFeature
         }
 
         if ($hasData) {
-            echo $Create->getJSONLDScript() . PHP_EOL;
+            return $content . $Create->getJSONLDScript() . PHP_EOL;
         }
     }
 
