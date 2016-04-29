946707f2089e3f72f419d4a726715580f540b342
Bas de Kort <bdekort@gmail.com>
2016-04-29 07:05:32 +0200

    Update: modifications to POT creation. This closes #50.

--- lib/Tools/CreatePot.php
+++ lib/Tools/CreatePot.php
@@ -6,12 +6,13 @@ namespace NextBuzz\SEO\Tools;
  * This is a helper class to retrieve all i18n:translate items from the xml files and converts them to an
  * empty POT file.
  *
- * Note: this is just a simple convertor and does not feature all possibilities of PO/POT files. Also it
- *       has some minor issues, so be warned :)
+ * Note: this is just a simle convertor and does not feature all possibilities of PO/POT files.
+ *
+ * Code borrowed from LengthOfRope\Treehouse\Utils
  *
  * @author LengthOfRope, Bas de Kort <bdekort@gmail.com>
  */
-class CreatePot
+class CreatePOT
 {
 
     /**
@@ -32,13 +33,12 @@ class CreatePot
         'xml' => array(
         ),
         'php' => array(
-            '#__\(["|\'](.*?)["|\'](.*)\)#' => array(1),
-            '#_e\(["|\'](.*?)["|\'](.*)\)#' => array(1),
-            '#_n\(["|\'](.*)["|\'], ["|\'](.*)["|\'], (.*)\)#U' => array(1, 2), // _n()
-            '#_n_noop\(["|\'](.*)["|\'], ["|\'](.*)["|\'], ["|\'](.*)["|\']\)#U' => array(1, 2), // _n_noop()
+            '#__\(["|\'](.*?)["|\'], (.*?)\)#' => array(1),
+            '#_e\(["|\'](.*?)["|\'], (.*?)\)#' => array(1),
+            '#_n\(["|\'](.*?)["|\'], ["|\'](.*?)["|\'], (.*?)\)#U' => array(1, 2), // _n()
+            '#_n_noop\(["|\'](.*?)["|\'], ["|\'](.*?)["|\'], ["|\'](.*?)["|\']\)#U' => array(1, 2), // _n_noop()
         )
     );
-    private $items = array();
     private $files = array(
         'xml' => array(),
         'php' => array()
@@ -50,21 +50,20 @@ class CreatePot
     private $useDom = array(
         'xml'
     );
-
     private $stripPathInComments = array();
-
     private $findings = array();
 
     /**
      * Add a path to add all xml files in that path
      * @param type $path
-     * @return \NextBuzz\L10N\CreatePot
+     * @return \NextBuzz\SEO\Tools\GeneratePOT
      */
     public function addPath($path, $ext = "xml")
     {
         if (!in_array($ext, $this->allowExt)) {
             throw new \Exception("Extension {$ext} is not allowed. Allowable: " . implode($this->allowExt, ", "), 500);
         }
+
         foreach ($this->globRecursive($path . "*." . $ext) as $file) {
             $this->files[$ext][] = str_replace('\\', '/', $file);
         }
@@ -72,13 +71,27 @@ class CreatePot
         return $this;
     }
 
+    /**
+     * Keep track of which paths to remove from the comments.
+     *
+     * @param string $path
+     * @return \NextBuzz\SEO\Tools\GeneratePOT
+     */
     public function stripPathInComments($path)
     {
         $this->stripPathInComments[] = str_replace('\\', '/', $path);
+
         return $this;
     }
 
-    // Does not support flag GLOB_BRACE
+    /**
+     * Loop through all dirs and return the files.
+     * Note: Does not support flag GLOB_BRACE
+     *
+     * @param string $pattern glob pattern
+     * @param int $flags
+     * @return array
+     */
     private function globRecursive($pattern, $flags = 0)
     {
         $files = glob($pattern, $flags);
@@ -93,57 +106,19 @@ class CreatePot
      * Parse all found files for i18n:translate strings
      * Note: Yup this is very nasty stupid code
      *
-     * @return \NextBuzz\L10N\CreatePot
+     * @return \NextBuzz\SEO\Tools\GeneratePOT
      */
     public function parse()
     {
-        $findings = array();
         foreach ($this->files as $ext => $files) {
             foreach ($files as $file) {
                 $content = file_get_contents($file);
                 if (in_array($ext, $this->useDom)) {
-                    // Use DomDocument
-                    libxml_use_internal_errors(true);
-                    $dom = new \DOMDocument();
-                    $dom->loadHTML($content);
-                    $xpath = new \DOMXPath($dom);
-                    $result = $xpath->query("//*[@*[local-name()='i18n:translate']]");
-                    $commentFile = str_replace($this->stripPathInComments, "", $file);
-                    foreach ($result as $node) {
-                        $translatable = $this->getNodeInnerData($node);
-                        if (!array_key_exists($translatable, $this->findings)) {
-                            $this->findings[$translatable] = array();
-                        }
-
-                        // Find linenumber
-                        $this->findings[$translatable][$node->getLineNo()] = $commentFile;
-                    }
-
-                } else {
-                    // Use regular exppressions
-                    $contentLines = explode("\n", $content);
-                    foreach ($this->lookFor[$ext] as $search => $matchIndexes) {
-                        preg_match_all($search, $content, $matches);
-                        if (isset($matches[0][0])) {
-                            foreach ($matchIndexes as $index) {
-                                foreach ($matches[$index] as $translatable) {
-                                    if (!array_key_exists($translatable, $this->findings)) {
-                                        $this->findings[$translatable] = array();
-                                    }
-
-                                    // Get linenumber
-                                    $commentFile = str_replace($this->stripPathInComments, "", $file);
-                                    $matchLines = preg_grep($search, $contentLines);
-                                    foreach (array_keys($matchLines) as $lineNumber) {
-                                        if (substr_count($matchLines[$lineNumber], $translatable) > 0) {
-                                            $this->findings[$translatable][($lineNumber+1)] = $commentFile;
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
+                    $this->getTranslationsUsingDomDoc($file, $content);
+                    continue;
                 }
+
+                $this->getTranslationsUsingRegExp($ext, $file, $content);
             }
         }
 
@@ -151,7 +126,80 @@ class CreatePot
     }
 
     /**
+     * Use the regexp matches to find translations
+     *
+     * @param string $content
+     */
+    private function getTranslationsUsingRegExp($ext, $file, $content)
+    {
+        // Use regular exppressions
+        $contentLines = explode("\n", $content);
+        foreach ($this->lookFor[$ext] as $search => $matchIndexes) {
+            preg_match_all($search, $content, $matches);
+            if (isset($matches[0][0])) {
+                foreach ($matchIndexes as $index) {
+                    foreach ($matches[$index] as $translatable) {
+                        if (!array_key_exists($translatable, $this->findings)) {
+                            $this->findings[$translatable] = array();
+                        }
+
+                        // Get linenumber
+                        $commentFile = str_replace($this->stripPathInComments, "", $file);
+                        $matchLines = preg_grep($search, $contentLines);
+                        foreach (array_keys($matchLines) as $lineNumber) {
+                            if (substr_count($matchLines[$lineNumber], $translatable) > 0) {
+                                $this->findings[$translatable][($lineNumber + 1)] = $commentFile;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Retrieve all TAL translations using DOMDocument.
+     *
+     * @param string $content
+     */
+    private function getTranslationsUsingDomDoc($file, $content)
+    {
+        // Use DomDocument
+        libxml_use_internal_errors(true);
+        $dom = new \DOMDocument();
+        $dom->loadHTML($content);
+        $xpath = new \DOMXPath($dom);
+        $result = $xpath->query("//*[@*[local-name()='i18n:translate']]");
+        $commentFile = str_replace($this->stripPathInComments, "", $file);
+        foreach ($result as $node) {
+            $translatable = $this->getNodeInnerData($node);
+            if (!array_key_exists($translatable, $this->findings)) {
+                $this->findings[$translatable] = array();
+            }
+
+            // Find linenumber
+            $this->findings[$translatable][$node->getLineNo()] = $commentFile;
+        }
+
+        // Add i18n:attributes support
+        $result2 = $xpath->query("//*[@*[local-name()='i18n:attributes']]");
+        foreach ($result2 as $node2) {
+            $translatableAttr = explode(";", $node2->getAttribute('i18n:attributes'));
+            foreach ($translatableAttr as $attr) {
+                $translatable = $node2->getAttribute(trim($attr));
+                if (!array_key_exists($translatable, $this->findings)) {
+                    $this->findings[$translatable] = array();
+                }
+
+                // Find linenumber
+                $this->findings[$translatable][$node2->getLineNo()] = $commentFile;
+            }
+        }
+    }
+
+    /**
      * Recursive loop through all child nodes to retrieve all content
+     *
      * @param \DOMElement $node
      */
     private function getNodeInnerData($node)
@@ -161,7 +209,7 @@ class CreatePot
 
         // Remove the 'outer node' with regular expressions
         $result = trim($dom->saveHTML());
-        $preg = array (
+        $preg = array(
             '#<' . $node->nodeName . '(.*?)>#',
             '#</' . $node->nodeName . '(.*?)>#'
         );
@@ -172,25 +220,24 @@ class CreatePot
 
     /**
      * Write an empty POT file containing all strings found
-     * @param type $file File to write to
-     * @param boolean $useParentThemeName If set to false, uses the child theme name and version
+     *
+     * @param string $file File to write to
      */
-    public function writePot($file, $useParentThemeName = true)
+    public function writePot($file)
     {
-        $output =
-            "# Copyright (C) " . date("Y") . " Next Buzz BV" . PHP_EOL .
+        $output = "# Copyright (C) " . date("Y") . " Next Buzz" . PHP_EOL .
             "msgid \"\"" . PHP_EOL .
             "msgstr \"\"" . PHP_EOL .
             "\"Project-Id-Version: buzz-seo\\n\"" . PHP_EOL .
-            "\"POT-Creation-Date: " . gmdate( 'Y-m-d H:i:s+0000' ). "\\n\"" . PHP_EOL .
+            "\"POT-Creation-Date: " . date("Y-m-d H:i:sO") . "\\n\"" . PHP_EOL .
             "\"MIME-Version: 1.0\\n\"" . PHP_EOL .
             "\"Content-Type: text/plain; charset=UTF-8\\n\"" . PHP_EOL .
             "\"Content-Transfer-Encoding: 8bit\\n\"" . PHP_EOL .
-            "\"PO-Revision-Date: ". gmdate( 'Y-m-d H:i:s+0000' ) ."\\n\"" . PHP_EOL .
-            "\"Language-Team: Next Buzz <info@nextbuzz.nl>\\n\"" . PHP_EOL . PHP_EOL;
+            "\"PO-Revision-Date: 2014-MO-DA HO:MI+ZONE\\n\"" . PHP_EOL .
+            "\"Language-Team: Next Buzz, Bas de Kort <bas@nextbuzz.nl>\\n\"" . PHP_EOL . PHP_EOL;
 
         require_once ABSPATH . '/wp-includes/pomo/po.php';
-        $Po = new \PO();
+        $poifyString = new \PO();
 
         foreach ($this->findings as $translatable => $locations) {
             $output .= "#:";
@@ -199,9 +246,11 @@ class CreatePot
             }
 
             $output .=
-                PHP_EOL . "msgid " . $Po->poify($translatable) .
+                PHP_EOL . "msgid " . $poifyString->poify(StringParser::factory($translatable)->trimMultiline()) .
                 PHP_EOL . "msgstr \"\"" . PHP_EOL . PHP_EOL;
         }
+
         file_put_contents($file, $output);
     }
+
 }
