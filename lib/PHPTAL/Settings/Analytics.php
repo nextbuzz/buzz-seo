e03fe3caca532f931a94418f3b7ad38ffcd3491b
Bas de Kort <bdekort@gmail.com>
2016-11-18 10:05:13 +0100

    Streamline admin interface

--- lib/PHPTAL/Settings/Analytics.php
+++ lib/PHPTAL/Settings/Analytics.php
@@ -12,6 +12,21 @@ class Analytics extends \NextBuzz\SEO\PHPTAL\SettingsPage
 
     public function __construct($tplName)
     {
+        // Make sure eventclicks is an before constructen the parent
+        $options = get_option('_settingsSettingsAnalytics', true);
+
+        if (!is_array($options) || !isset($options['eventsclicks']) || !is_array($options['eventsclicks']))
+        {
+            if (!is_array($options)) {
+                $options = array();
+            }
+            $options['eventsclicks'] = array();
+
+            // Make sure we re-retrieve the new settings
+            wp_cache_delete('_settingsSettingsAnalytics');
+            update_option('_settingsSettingsAnalytics', $options);
+        }
+
         parent::__construct('SettingsAnalytics');
     }
 }
